a4633aa59b2d1d5a53c88869575bf69e23200282
Vineela Jella <vineelajella@gmail.com>
2024-11-11 13:36:30 +0530

    Your commit message here

--- program1.py
+++ program1.py
@@ -2,5 +2,54 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
+    
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        island_count = 0
+
+        def dfs(r, c):
+            
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 'W':
+                return
+            
+            grid[r][c] = 'W'
+           
+            dfs(r - 1, c)  
+            dfs(r + 1, c) 
+            dfs(r, c - 1)  
+            dfs(r, c + 1)  
+
+       
+        for r in range(rows):
+            for c in range(cols):
+        
+                if grid[r][c] == 'L':
+                    island_count += 1
+                    dfs(r, c) 
+
+        return island_count
+
+
+solution = Solution()
+
+grid1 = [
+    ["L", "L", "L", "L", "W"],
+    ["L", "L", "W", "L", "W"],
+    ["L", "L", "W", "W", "W"],
+    ["W", "W", "W", "W", "W"]
+]
+
+grid2 = [
+    ["L", "L", "W", "W", "W"],
+    ["L", "L", "W", "W", "W"],
+    ["W", "W", "L", "W", "W"],
+    ["W", "W", "W", "L", "L"]
+]
+
+print(solution.getTotalIsles(grid1))  
+print(solution.getTotalIsles(grid2))  
+
+    
                     
-        return 0
--- program2.py
+++ program2.py
@@ -1,5 +1,40 @@
 def decode_message( s: str, p: str) -> bool:
 
 # write your code here
+
+    
+    i, j = 0, 0
+    star_idx = -1  
+    match_idx = 0  
+
+    while i < len(s):
+        if j < len(p) and (p[j] == '?' or s[i] == p[j]):
+           
+            i += 1
+            j += 1
+        elif j < len(p) and p[j] == '*':
+           
+            star_idx = j
+            match_idx = i
+            j += 1
+        elif star_idx != -1:
+            
+            j = star_idx + 1
+            match_idx += 1
+            i = match_idx
+        else:
+            return False
+
+    while j < len(p) and p[j] == '*':
+        j += 1
+
   
-        return False
+    return j == len(p)
+
+
+print(decode_message("aa", "a"))  
+print(decode_message("aa", "*"))  
+print(decode_message("cb", "?a"))  
+print(decode_message("aa", "a*"))  
+
+  
